e00e1a6093b1fefe51e17f3281f8dd9c6f53ced8
srvanderplas <srvanderplas@gmail.com>
2013-05-15 13:52:29 -0500

    update git script so that it auto pulls from github and so that it only runs one file.

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd /home/susan/Documents/R\ Projects/Craigslist
-R CMD BATCH ./ScrapeCLForSale.R
+git pull
+# R CMD BATCH ./ScrapeCLForSale.R
 R CMD BATCH ./ScrapeCL.R
 git commit -a -m "CRON Auto-Commit after script runs"
 git push

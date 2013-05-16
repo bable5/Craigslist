cd436b34d2fe0e3740a93e1d50a99fdf980617b7
srvanderplas <srvanderplas@gmail.com>
2013-05-16 08:42:35 -0500

    Changes in analysis, bugfixes, timezones.

--- AnalyzeMIS.R
+++ AnalyzeMIS.R
@@ -1,38 +1,38 @@
 library(scrapeR)
 library(plyr)
 library(reshape2)
+library(lubridate)
+library(multicore)
+library(stringr)
 
 setwd("/home/susan/Dropbox/GraphicsGroup/Craigslist/")
+source("./timezones.R")
+timezones.conv <- unique(timezones[,c("Abbr.", "trunc")])
+
 url <- "http://www.craigslist.org/about/sites"
 
-cities <- scrape(url)[[1]]
-regions <- getNodeSet(cities, '//*[@id="index"]/div[@class="colmask"]')
-
-getRegionInfo <- function(i){
-  region.name = xmlAttrs(xmlChildren(xmlChildren(regions[[i]])$h1)$a)
-  state.names = xpathApply(regions[[i]], path=paste('//*[@id="index"]/div[@class="colmask"][', i, ']/div/div/div/div/div[@class="state_delimiter"]', sep=""), xmlValue)
-  state.subs = xpathApply(regions[[i]], path=paste('//*[@id="index"]/div[@class="colmask"][', i, ']/div/div/div/div/ul', sep=""), xmlChildren)
-  cl.urls = as.character(unlist(xpathApply(regions[[i]], path=paste('//*[@id="index"]/div[@class="colmask"][', i, ']/div/div/div/div/ul/li/a', sep=""), xmlAttrs)))
-  cl.names = as.character(unlist(xpathApply(regions[[i]], path=paste('//*[@id="index"]/div[@class="colmask"][', i, ']/div/div/div/div/ul/li', sep=""), xmlValue)))
-  df <- do.call("rbind", lapply(1:length(state.names), function(j){
-    cl.name = sapply(state.subs[[j]], xmlValue)
-    reps = sum(names(cl.name)=="li")
-    return(data.frame(state=rep(as.character(state.names[j]), reps), region=rep(as.character(region.name), reps), stringsAsFactors=FALSE))
-  } )) 
-  df$urls <- cl.urls
-  df$names <- cl.names
-  df
-}
-
-craigslistURLs <- do.call("rbind", lapply(1:length(regions), getRegionInfo))
-
-craigslistURLs <- craigslistURLs[which(craigslistURLs$region%in%c("US", "CA")),]
-
 data <- read.csv("./CL-mis.csv", stringsAsFactors=FALSE)
+data <- apply(data, 2, str_trim)
+data <- apply(data, 2, function(i) {
+                i[str_length(i)==0] <- NA
+                return(i)})
+data <- as.data.frame(data, stringsAsFactors=FALSE)
 
-library(lubridate)
 data$timezone <- substr(data$datetime, 21, 23)
-data$datetime <- strptime(substr(data$datetime, 1, 19), format="%Y-%m-%d, %I:%M%p")
-attr(data$datetime, "tzone") <- data$timezone
-temp <- strptime(data$datetime[1:20], format="%Y-%m-%d, %I:%M%p")
-force_tz(temp, data$timezone[1:20])
+data$timezone[is.na(data$timezone)] <- "UTC"
+timezoneidx <- unlist(mclapply(data$timezone, function(i) which(timezones.conv$trunc==i)[1]))
+tz.tab <- timezones.conv[unique(timezoneidx),]
+data$timezone <- timezones.conv$Abbr.[timezoneidx]
+
+data$datetime2 <- unlist(mclapply(1:nrow(data), function(i) strftime(substr(data$datetime[i], 1, 19), format="%Y-%m-%d, %I:%M%p", tz=data$timezone[i], usetz=TRUE)))
+
+data$lat[data$lat==0] <- NA
+data$long[data$long==0] <- NA
+
+data <- data[,apply(data, 2, function(i) sum(!is.na(i)))!=0]
+
+# correlation matrix of missingness
+cor.melt <- melt(cor(is.na(data)))
+qplot(data=cor.melt, x=Var1, y=Var2, geom="tile", fill=value) + 
+  scale_fill_gradient2(limits=c(-1.1,1.1))
+cor.melt[which(cor.melt$value< -0.5),]
